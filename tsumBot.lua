3130bd1265abc7cda6d0b3e125ff1e43bc72de79
Siew Kar Fai <karfai0317@gmail.com>
2016-05-29 17:41:48 +0800

    able to set claim all at start

--- tsumBot.lua
+++ tsumBot.lua
@@ -11,7 +11,13 @@ require("tsumBot.actions")
 -- sendHearts(isStopAtZero)
 -- standBy(duration)
 
-claimAll()
+local label = {type=CONTROLLER_TYPE.LABEL, text="Choose you settings"}
+local claimAll_at_start_switch = {type=CONTROLLER_TYPE.SWITCH, title="Claim all at start:", key="claimAll_at_start", value=0}
+local controls = {label, claimAll_at_start_switch}
+local enableRemember = true
+dialog(controls, enableRemember)
+
+if claimAll_at_start_switch.value == 1 then claimAll() end
 
 while true do
     claimIndividually()
